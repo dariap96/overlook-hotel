8b5c297a0724fbf033a68683151104c74c5e095a
вфк <284350@niuitmo.ru>
2022-10-15 12:41:09 +0300

    Initial commit

---
+++ src/main/java/itmo/com/overlook/hotel/OverlookHotelApplication.java
@@ -0,0 +1,13 @@
+package itmo.com.overlook.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverlookHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverlookHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/tests/itmo/com/overlook/hotel/OverlookHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.com.overlook.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlookHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
