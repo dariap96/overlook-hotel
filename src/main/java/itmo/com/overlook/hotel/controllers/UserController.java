08b51ceed54bc34e9e3ccd4dd2b854d530101f3c
вфк <284350@niuitmo.ru>
2022-10-18 09:31:34 +0300

    code clean up

--- src/main/java/itmo/com/overlook/hotel/controllers/UserController.java
+++ src/main/java/itmo/com/overlook/hotel/controllers/UserController.java
@@ -7,10 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/users")
