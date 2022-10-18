520e92cbf6cd38f3f3bdd77f3ac9c160ea670f66
Felix Ardi <sonik-gx@mail.ru>
2022-10-18 19:24:06 +0300

    Proxy added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    rooms: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/rooms');
+    const body = await response.json();
+    this.setState({rooms: body});
+  }
+
+  render() {
+    const {rooms} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <div className="App-intro">
+              <h2>Rooms</h2>
+              {rooms.map(room =>
+                  <div key={room.id}>
+                    {room.number}
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
