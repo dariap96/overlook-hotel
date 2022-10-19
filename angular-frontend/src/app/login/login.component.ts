6589ce47d70970c46e4be0f36df2176ef958148b
вфк <284350@niuitmo.ru>
2022-10-19 03:25:30 +0300

    login works

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -40,7 +40,6 @@ export class LoginComponent implements OnInit {
           this.isLoginFailed = false;
           this.isLoggedIn = true;
           this.roles = this.tokenStorage.getUser().roles;
-          this.reloadPage();
         },
         err => {
           this.errorMessage = err.error.message;
