30341da74c66256b86f0c5672ab3e370c70ae609
Nastya <martynenkoav.18@mail.ru>
2022-10-31 20:01:05 +0300

    trying to figure out booking

---
+++ src/main/java/itmo/com/overlook/hotel/entities/RoomDate.java
@@ -0,0 +1,29 @@
+package itmo.com.overlook.hotel.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "room_dates")
+public class RoomDate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @Convert(disableConversion = true)
+    @Column(name = "date")
+    private LocalDate date;
+
+
+
+}
